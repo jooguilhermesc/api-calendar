2b18dd80a60209f18145a31e9e6dc1009e2d63c4
João Guilherme Silva Cabral <46795337+jooguilhermesc@users.noreply.github.com>
2022-10-19 11:42:00 -0300

    atualizações para  definir dias úteis e feriados

--- utils.py
+++ utils.py
@@ -1,26 +1,19 @@
-from xml.dom.minidom import Element
-from xml.sax.handler import DTDHandler
 import pandas as pd
 from datetime import datetime
 
 class workdays_calendar:
     
     def __init__(self, start='2017-12-01', end = datetime.today().strftime('%Y-%m-%d')):
-        self.start = start #'2000-01-01'
-        self.end = end #'2050-12-31'
-        self.url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTtYOgUPr8G8kWqXlqXDZ_-6_0mzPxGq_WPUqglOan50uUYa8L1QA1E9ZjxAWqNLGBhUX3YxleOw_ja/pub?gid=0&single=true&output=csv'
+        self.start = start 
+        self.end = end 
+        self.url = 'calendario.csv' #'https://docs.google.com/spreadsheets/d/e/2PACX-1vTtYOgUPr8G8kWqXlqXDZ_-6_0mzPxGq_WPUqglOan50uUYa8L1QA1E9ZjxAWqNLGBhUX3YxleOw_ja/pub?output=csv'
         
-    def __holidays__(self):
+    def holidays(self):
         holidays = pd.read_csv(self.url,encoding='utf-8')
         df = list(holidays["dt_feriado"])
 
         return df
     
-    def __nmholidays__(self):
-        df = pd.read_csv(self.url,encoding='utf-8')
-        
-        return df
-
     def create_date_table(self):
         df = pd.DataFrame({"Date": pd.date_range(self.start, self.end)})
         df["Day"] = df.Date.dt.dayofweek
@@ -30,9 +23,7 @@ class workdays_calendar:
         df["Year"] = df.Date.dt.year
         df["Year_half"] = (df.Quarter + 1) // 2
         
-        nm_feriados = self.__nmholidays__()
-        lista_feriados = self.__holidays__()
-        df["workday"] = df["Date"].apply(lambda x: 0 if x.strftime('%d/%m') in lista_feriados else 1)
-        df["workday"] = df["Day"].apply(lambda x: 0 if x in (0,6) else 1)
+        lista_feriados = self.holidays()
+        df["workday"] = df.apply(lambda x: 0 if (x.Date.strftime('%d-%m') in lista_feriados) or (x.Day in (0,6)) else 1,axis=1)
             
         return df
